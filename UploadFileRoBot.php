49f2df0c07ec28094fe1a9e449c0183465e2978f
mohmmadreza0021 <73956958+mohmmadreza0021@users.noreply.github.com>
2025-02-25 09:25:13 -0800

    Update UploadFileRoBot.php

--- UploadFileRoBot.php
+++ UploadFileRoBot.php
@@ -5,7 +5,7 @@
      @oYSoF
      */
 
-     define('BOT_TOKEN','332987003:AAE32beMm5Th0P-dujEZ2BIJsv8408nLosw');
+     define('BOT_TOKEN','8152621696:AAHeP8gA3q7VoFr4kgPOySgCh-DADyr57GY');
      $update = json_decode(file_get_contents('php://input'));
      $chat_id = $update->message->chat->id;
      $msg_id = $update->message->message_id;
